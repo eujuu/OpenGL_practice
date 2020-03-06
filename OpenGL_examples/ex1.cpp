426876ff402243ce01690194480d49ec7ceed05b
eujuu <e97214j@naver.com>
2020-03-06 12:13:50 +0900

    setting the color of window

--- OpenGL_examples/ex1.cpp
+++ OpenGL_examples/ex1.cpp
@@ -13,18 +13,12 @@ const unsigned int SCR_HEIGHT = 600;
 int main()
 {
 	// glfw: initialize and configure
-	// ------------------------------
 	glfwInit();
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
 	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
-#ifdef __APPLE__
-	glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE); // uncomment this statement to fix compilation on OS X
-#endif
-
 	// glfw window creation
-	// --------------------
 	GLFWwindow* window = glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, "LearnOpenGL", NULL, NULL);
 	if (window == NULL)
 	{
@@ -36,7 +30,6 @@ int main()
 	glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
 
 	// glad: load all OpenGL function pointers
-	// ---------------------------------------
 	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
 	{
 		std::cout << "Failed to initialize GLAD" << std::endl;
@@ -44,35 +37,32 @@ int main()
 	}
 
 	// render loop
-	// -----------
 	while (!glfwWindowShouldClose(window))
 	{
 		// input
-		// -----
-		processInput(window);
+		processInput(window); //렌더링 루프 반복될때 마다 processInput함수 호출
 
-		// glfw: swap buffers and poll IO events (keys pressed/released, mouse moved etc.)
-		// -------------------------------------------------------------------------------
+		//이 위치에 렌더링 명령
+		glClearColor(0.3f, 0.3f, 0.5f, 0.7f); // 컬러버퍼 설정, 어떤 색으로 화면을 지울 것인지., 상태설정 함수
+		glClear(GL_COLOR_BUFFER_BIT); //어떠한 색으로 지울 것인지 검색하는데에 현재 상태 사용 -> 상태 사용 함수
+		// 이벤트 확인하고 버퍼 교체
+		glfwPollEvents(); 
 		glfwSwapBuffers(window);
-		glfwPollEvents();
 	}
 
 	// glfw: terminate, clearing all previously allocated GLFW resources.
-	// ------------------------------------------------------------------
 	glfwTerminate();
 	return 0;
 }
 
-// process all input: query GLFW whether relevant keys are pressed/released this frame and react accordingly
-// ---------------------------------------------------------------------------------------------------------
+// 키보드 키와  윈도우 입력을 함께 받음, 
 void processInput(GLFWwindow *window)
 {
-	if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
-		glfwSetWindowShouldClose(window, true);
+	if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS) //해당키가 눌려져 잇는지 판단
+		glfwSetWindowShouldClose(window, true); //window 닫음
 }
 
 // glfw: whenever the window size changed (by OS or user resize) this callback function executes
-// ---------------------------------------------------------------------------------------------
 void framebuffer_size_callback(GLFWwindow* window, int width, int height)
 {
 	// make sure the viewport matches the new window dimensions; note that width and 
